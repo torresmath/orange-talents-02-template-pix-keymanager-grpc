566c209858e5a2b2eb3abd1e2f98ccf89c863cfa
matheus.torres <torresmatheus_@hotmail.com>
2021-04-13 09:36:09 -0300

    SOLVED 01 - more tests

--- src/test/kotlin/br/com/torresmath/key/manager/deleteKey/DeleteKeyEndpointTest.kt
+++ src/test/kotlin/br/com/torresmath/key/manager/deleteKey/DeleteKeyEndpointTest.kt
@@ -6,6 +6,7 @@ import br.com.torresmath.key.manager.DeleteKeyRequest
 import br.com.torresmath.key.manager.KeyType
 import br.com.torresmath.key.manager.generateKey.PixKey
 import br.com.torresmath.key.manager.generateKey.PixKeyRepository
+import com.google.protobuf.Empty
 import io.grpc.ManagedChannel
 import io.grpc.Status
 import io.grpc.StatusRuntimeException
@@ -15,11 +16,11 @@ import io.micronaut.grpc.annotation.GrpcChannel
 import io.micronaut.grpc.server.GrpcServerChannel
 import io.micronaut.test.annotation.MockBean
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
-import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.mock
 import javax.inject.Inject
@@ -33,30 +34,66 @@ internal class DeleteKeyEndpointTest(
     @field:Inject
     lateinit var pixKeyRepository: PixKeyRepository
 
-    val defaultPixKey =
-        PixKey("c56dfef4-7901-44fb-84e2-a2cefb157890", KeyType.CPF, "42549789873", AccountType.CHECKING_ACCOUNT)
-
     @BeforeEach
     internal fun setUp() {
-        pixKeyRepository.save(defaultPixKey)
+        pixKeyRepository.update(defaultPixKey)
     }
 
     @AfterEach
     internal fun tearDown() = pixKeyRepository.deleteAll()
 
-    @Test
-    fun `should return NOT_FOUND`() {
+    companion object {
+        val defaultPixKey = PixKey(
+            "c56dfef4-7901-44fb-84e2-a2cefb157890",
+            KeyType.CPF,
+            "42549789873",
+            AccountType.CHECKING_ACCOUNT
+        )
+
+        val clientId = defaultPixKey.clientId
+        val pixId = defaultPixKey.pixUuid
+
+        @JvmStatic
+        fun notFoundParams() = listOf(
+            Arguments.of(clientId, ""),
+            Arguments.of("", pixId),
+            Arguments.of("invalid", "id"),
+            Arguments.of("", "")
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("notFoundParams")
+    fun `should return NOT_FOUND`(clientId: String, pixId: String) {
 
         val request = DeleteKeyRequest.newBuilder()
-            .setClientId("invalid")
-            .setPixId("id")
+            .setClientId(clientId)
+            .setPixId(pixId)
             .build()
 
         val exc = assertThrows<StatusRuntimeException> { blockingStub.deleteKey(request) }
         assertEquals(Status.NOT_FOUND.code, exc.status.code)
     }
+
+    @Test
+    fun `should delete successfully and return EMPTY`() {
+
+        val request = DeleteKeyRequest.newBuilder()
+            .setClientId(defaultPixKey.clientId)
+            .setPixId(defaultPixKey.pixUuid)
+            .build()
+
+        val response = assertDoesNotThrow { blockingStub.deleteKey(request) }
+        assertEquals(Empty.newBuilder().build(), response)
+    }
 }
 
+
+/**
+ * I wasn't sure how to reproduce a scenario with duplicated entries in db
+ * (even manually is hard to introduce this bug in the project w/o changing PixKey model)
+ * so I mocked a query retrieving duplicated data
+ */
 @MicronautTest
 class DeleteKeyEndpointUnitTest(
     @field:Inject
@@ -80,10 +117,22 @@ class DeleteKeyEndpointUnitTest(
 
         `when`(
             mockRepository.findByClientIdAndPixUuid(request.clientId, request.pixId)
-        ).thenReturn(mutableListOf(
-            PixKey("c56dfef4-7901-44fb-84e2-a2cefb157890", KeyType.MOBILE_NUMBER, "+5511972651418", AccountType.CHECKING_ACCOUNT),
-            PixKey("c56dfef4-7901-44fb-84e2-a2cefb157890", KeyType.MOBILE_NUMBER, "+5511972651418", AccountType.CHECKING_ACCOUNT)
-        ))
+        ).thenReturn(
+            mutableListOf(
+                PixKey(
+                    "c56dfef4-7901-44fb-84e2-a2cefb157890",
+                    KeyType.MOBILE_NUMBER,
+                    "+5511972651418",
+                    AccountType.CHECKING_ACCOUNT
+                ),
+                PixKey(
+                    "c56dfef4-7901-44fb-84e2-a2cefb157890",
+                    KeyType.MOBILE_NUMBER,
+                    "+5511972651418",
+                    AccountType.CHECKING_ACCOUNT
+                )
+            )
+        )
 
         val exc = assertThrows<StatusRuntimeException> { blockingStub.deleteKey(request) }
         assertEquals(Status.INTERNAL.code, exc.status.code)
